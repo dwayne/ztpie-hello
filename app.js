7479679e953ef26e58fd25e428902c4c605cc670
Dwayne Crooks <me@dwaynecrooks.com>
2024-10-07 08:18:20 -0400

    Update app.js and index.css

--- app.js
+++ app.js
@@ -4437,7 +4437,7 @@ var $author$project$Main$main = A2(
 	_List_Nil,
 	_List_fromArray(
 		[
-			$elm$html$Html$text('Hey, '),
+			$elm$html$Html$text('Hi, '),
 			A2(
 			$elm$html$Html$span,
 			_List_fromArray(
