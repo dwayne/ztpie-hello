5b8cc721f776f8869330032a812fafe68e5c36cd
Dwayne Crooks <me@dwaynecrooks.com>
2024-10-19 07:04:05 -0400

    Say "Hey" and increase the font size

--- src/Main.elm
+++ src/Main.elm
@@ -6,7 +6,7 @@ import Html.Attributes as HA
 main : H.Html msg
 main =
     H.p []
-        [ H.text "Hello, "
+        [ H.text "Hey, "
         , H.span [ HA.class "name" ] [ H.text "Dwayne" ]
         , H.text "!"
         ]
